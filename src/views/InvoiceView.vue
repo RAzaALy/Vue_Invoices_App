4148c12ebefb3d05b746f0cf8e60eda920e1bab9
raza <razaaly0007@gmail.com>
2022-04-25 21:05:54 +0500

    update

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -113,7 +113,7 @@ export default {
   methods: {
     ...mapMutations(["SET_CURRENT_INVOICE", "TOGGLE_EDIT_INVOICE", "TOGGLE_INVOICE"]),
 
-    ...mapActions(["DELETE_INVOICE", "UPDATE_STATU3S_TO_PENDING", "UPDATE_STATUS_TO_PAID"]),
+    ...mapActions(["DELETE_INVOICE", "UPDATE_STATUS_TO_PENDING", "UPDATE_STATUS_TO_PAID"]),
 
     getCurrentInvoice() {
       this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
